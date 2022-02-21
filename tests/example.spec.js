314c08adac3d2b706bc319601b3242ca7f5dc372
unknown <syedyousafraza@yahoo.com>
2022-02-21 13:01:40 +0500

    update in file

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,6 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
-//adding test
+//adding test Just to test
 
 test.beforeEach(async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc');
